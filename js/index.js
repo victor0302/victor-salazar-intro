1b33f42e7b705b3f703a3945038c8562b7cadc47
Vic <victorsalazar.01.vv@gmail.com>
2026-05-19 12:44:12 -0600

    js added, created footer and skills

--- js/index.js
+++ js/index.js
@@ -10,8 +10,20 @@ const thisYear = today.getFullYear();
 
 const copyright = document.createElement('p');
 
-copyright.innerHTML = "Victor Salazar " + thisYear;
+copyright.innerHTML = "Victor Salazar © " + thisYear;
 
 footer.appendChild(copyright);
 
 
+const skills = ['JavaScript', 'HTML', 'CSS', 'Python', 'SQL'];
+
+const skillsSection = document.getElementById("Skills")
+
+const skillsList = skillsSection.querySelector("ul")
+
+
+for (let i =0 ; i<skills.length;i++){
+    let skill = document.createElement('li')
+    skill.innerText = skills[i]
+    skillsList.appendChild(skill)
+}
